2c3331967d001beb0da1c4f0cd40a954e2287fd5
Osama Yousry <osama.yousry@outlook.com>
2016-05-14 23:16:54 +0200

    Added pin array and motor array instead of individual definitions

--- ZUArm.ino
+++ ZUArm.ino
@@ -8,48 +8,29 @@ theta2 Second Motor is connected to same pin => 11
 theta3 => 3
 theta4 => 5
 */
-int theta1Pin = 9;
-int theta2Pin = 11;
-int theta3Pin = 3;
-int theta5Pin = 5;
-int openGrip;
-
+int thetaPin[] = {9,11,3,5};
+const int numberOfMotors = 4;
 //We define a servo instance for each motor
-Servo theta1Servo;
-Servo theta2Servo;
-Servo theta3Servo;
-Servo theta4Servo;
+Servo thetaServo[numberOfMotors];
 
 void setup() {
   //Initialize Serial Com
   Serial.begin(9600);
   //Attach each instance with the corresponding pin
-  theta1Servo.attach(theta1Pin);
-  theta2Servo.attach(theta2Pin);
-  theta3Servo.attach(theta3Pin);
-  theta4Servo.attach(theta4Pin);
+  for (int i; i < numberOfMotors; i++) {
+    thetaServo[i].attach(i);
+  }
 }
-//Variables that will be sent by user
-int theta1,theta2,theta3,theta4;
+int Angle;
 void loop() {
   //When the serial buffer contains the data we parse the data to send to the
   //motors, data is sent as follows xx yy zz w
   //Where xx is theta1 and yy is theta2 and zz is theta3 and w is the grip condition
-  if (Serial.available()) {
-   theta1 = Serial.readStringUntil(' ').toInt();
-   theta2 = Serial.readStringUntil(' ').toInt();
-   theta3 = Serial.readStringUntil(' ').toInt();
-   //This was to fix a bug from matlab
-   while (!Serial.available());
-   openGrip = Serial.parseInt();
-  }
-  //We send the angles to the motors
-  theta1Servo.write(theta1);
-  theta2Servo.write(theta2);
-  theta3Servo.write(theta3);
-  //When the arm reaches its final destiantion we change the grip mode
-  if (theta1Servo.read() == theta1 && theta2Servo.read() == theta2 && theta3Servo.read() == theta3) {
-   if (openGrip == 1) {theta4Servo.write(180); }
-   else { theta4Servo.write(0); }
+  while (Serial.available()) {
+   int servoNo = Serial.readStringUntil(' ').toInt();
+   if (servoNo < numberOfMotors) {
+     Angle = Serial.readStringUntil(':').toInt();
+     thetaServo[servoNo].write(Angle);
+   }
   }
 }
