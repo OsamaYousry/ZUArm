135355078eda858a15cd6c71b58643eee9529104
m7md7sien <m7md.7ssien@gmail.com>
2016-05-18 20:25:27 +0200

    stil need modifications for pitch angle constraints

--- fiveDOF.m
+++ fiveDOF.m
@@ -4,11 +4,11 @@ clear all
 
 %% setup link lengths
 
-L1 = 20; %the link length
+L1 = 5; %the link length
 L1str = num2str(L1);
-L2 = 20; 
+L2 = 5; 
 L2str = num2str(L2);
-L3 = 20; 
+L3 = 5; 
 L3str = num2str(L3);
 
 Lmax = L1 + L2 + L3;
@@ -51,9 +51,18 @@ if (~isempty(x))
     else
         zminLimit = -sqrt((L2+L3)^2 - (L1-r)^2);
     end
+    
+    % solve approximation errors
+    if ~isreal(zminLimit)
+        zminLimit = 0;
+    end
+    if ~isreal(zmaxLimit)
+        zmaxLimit = 0;
+    end
+    
     zminLimitstr = num2str(zminLimit);
     
-    str = strcat('enter z value in range [-', zminLimitstr, ', ', zmaxLimitstr, ']\n');
+    str = strcat('enter z value in range [', zminLimitstr, ', ', zmaxLimitstr, ']\n');
     z = input(str);
     while(isempty(z) || (z < zminLimit) || (z > zmaxLimit))
         z = input(str);
@@ -82,24 +91,23 @@ end
 %% read pitch angle
 
 if (~isempty(x))
-    pitchmax1 = acos((r+L1+L2) / L3);
-    pitchmin1 = acos((r - L1 - L2) / L3);
     
-    pitchmax2 = asin((z+L2)/L3);
-    pitchmin2 = asin((z-L1-L2)/L3); 
-    
-    pitchmax = min(pitchmax1, pitchmax2)*(180/pi);
-    pitchmin = max(pitchmin1, pitchmin2)*(180/pi);
-    
-    pitchmaxstr = num2str(pitchmax);
-    pitchminstr = num2str(pitchmin);
-    
-    str = strcat('enter the angle of pitch orientation in range [', pitchminstr, ', ', pitchmaxstr, ']\n');
+    str = strcat('enter the angle of pitch orientation in range [0, 360]\n');
     pitch = input(str);
-    while(isempty(pitch) || gripper < 0 || gripper > 360) %accept all values for testing
+    while(isempty(pitch) || pitch < 0 || pitch > 360) %accept all values for testing
         pitch = input(str);
     end
-    pitch = pitch * (pi/180);
+    pitch = pitch * (pi/180); %degres to radian
+    
+    %check pitch limits -not reliable and need modification-
+    pitchmax1 = acos((r+L1+L2) / L3);
+    pitchmin1 = acos((r - L1 - L2) / L3);
+
+    pitchmax2 = asin((z+L2)/L3);
+    pitchmin2 = asin((z-L1-L2)/L3);
+        
+    pitchmax = min(pitchmax1, pitchmax2);
+    pitchmin = max(pitchmin1, pitchmin2);
 end
 
 
@@ -108,38 +116,80 @@ errorFlag = 0;
 
 outstr= '';
 if ~isempty(x)
-    theta0 = round(atan2(y,x)*(180/pi));
-    theta2 = round(acos(((r-L3*cos(pitch))^2+(z-L3*sin(pitch))^2-L1^2-L2^2)/(2*L1*L2))*(180/pi));
-    theta1 = round(asin(((z-L3*sin(pitch))*(L1+L2*cos(theta2))-L2*sin(theta2)*(r-L3*cos(pitch)))/((r-L3*cos(pitch))^2+(z-L3*sin(pitch))^2))*(180/pi));
+    
+    if y < 0
+        theta0 = atan2(-y,-x);
+    else
+        theta0 = atan2(y,x);
+    end
+    
+    theta2 = acos(((r-L3*cos(pitch))^2+(z-L3*sin(pitch))^2-L1^2-L2^2)/(2*L1*L2));
+    theta1 = asin(((z-L3*sin(pitch))*(L1+L2*cos(theta2))-L2*sin(theta2)*(r-L3*cos(pitch)))/((r-L3*cos(pitch))^2+(z-L3*sin(pitch))^2));
+    if y < 0
+        theta1 = pi - theta1;
+    end
+    
+    % check for errors due to pitch out of range
+    if ~isreal(theta2) || ~isreal(theta1) || ~isreal(theta0)
+        errorFlag = 1;
+        fprintf('pitch angle out of range, chose another angle');
+    end
+    
     theta3 = pitch - theta2 - theta1;
-    outstr = strcat(outstr, {'0 '},num2str(theta0), {':1 '},int2str(theta0),':');
-    % check angle constraints
-    if (180 < theta0 || 0 > theta0)
-        errorflag = 1;
-        fprintf('theta0 not in limts = %i\n', theta0);
+                    
+    % check constraints
+    
+    if ~errorFlag %&& ((pitchmin < pitch) && (pitch < pitchmax))
+        %check for joints angles constraints
+        if (pi < theta0 || 0 > theta0)
+            errorFlag = 1;
+        end
+    
+        if (pi < theta1 || 0 > theta1)
+            errorFlag = 1;
+        end
+    
+        if (0 < theta2 || (-(2/3)*pi) > theta2)
+            errorFlag = 1;
+        end
+    
+        if (0 < theta3 || (-(2/3)*pi) > theta3)
+            errorFlag = 1;
+        end
+        
+        if ~errorFlag 
+            %forward kinematics for douple check
+            pitchC = theta1+theta2+theta3;
+            rC = L1*cos(theta1) + L2*cos(theta1+theta2) + L3*cos(pitchC);
+            zC = L1*sin(theta1) + L2*sin(theta1+theta2) + L3*sin(pitchC);
+            xC = rC*cos(theta0);
+            yC = rC*sin(theta0);
+            
+            if theta1 > pi/2
+                xC = -xC;
+                yC = -yC;
+            end
+            
+            %consider small error due to approximation
+            if(abs(pitchC-pitch)>0.1 || abs(zC-z)>0.1 || abs(xC-x)>0.1 || abs(yC-y)>0.1 || ~isreal(zC) || ~isreal(xC) || ~isreal(yC))
+                fprintf('problem \n')
+                errorFlag = 1;
+            end
+        end
     end
+    %radian to degres to be suitable for servo motor
+    theta0 = round(theta0 * (180/pi));
+    theta1 = round(theta1 * (180/pi));
+    theta2 = round(theta2 * (180/pi));
+    theta3 = round(theta3 * (180/pi));
     
-    if (120 < theta1 || 0 > theta1)
-        errorflag = 1;
-        fprintf('theta1 not in limts = %i\n', theta1);
-    end
-    
-    if (120 < theta2 || 0 > theta2)
-        errorflag = 1;
-        fprintf('theta2 not in limts = %i\n', theta2);
-    end
-    
-    if (120 < theta3 || 0 > theta3)
-        errorflag = 1;
-        fprintf('theta3 not in limts = %i\n', theta3);
-    end
-    
-    
+    %output string that should be sent to arduino
+    outstr = strcat(outstr, {'0 '},num2str(theta0), {':1 '},int2str(theta1), {':2 '},int2str(theta2), {':3 '},int2str(theta3),':');
 end
 
 if ~isempty(phi)
     theta4 = round(phi);
-    outstr = strcat({'4 '}, num2str(theta4), ':');
+    outstr = strcat(outstr, {'4 '}, num2str(theta4), ':');
 end
 
 if ~isempty(gripper)
@@ -152,7 +202,9 @@ end
 
 delete(instrfind)
 global S
-S = serial('COM28','BaudRate',9600,'timeOut',0.1);
-fopen(S);
-fprintf(S,outstr);
-fclose(S);
+if ~errorFlag
+    S = serial('COM28','BaudRate',9600,'timeOut',0.1);
+    fopen(S);
+    fprintf(S,outstr);
+    fclose(S);
+end
