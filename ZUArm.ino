bc7cbe6524fd19d9e0b7b3d91d44576b5fe7ee52
Osama Yousry <osama.yousry@outlook.com>
2016-06-10 04:19:56 +0200

    Fixed to work with 6DOF and to read values from the servos and print them to serial

--- ZUArm.ino
+++ ZUArm.ino
@@ -8,8 +8,8 @@ theta2 Second Motor is connected to same pin => 11
 theta3 => 3
 theta4 => 5
 */
-int thetaPin[] = {9,11,3,5};
-const int numberOfMotors = 4;
+int thetaPin[] = {3,5,6,9,10,11};
+const int numberOfMotors = 6;
 //We define a servo instance for each motor
 Servo thetaServo[numberOfMotors];
 
@@ -17,8 +17,11 @@ void setup() {
   //Initialize Serial Com
   Serial.begin(9600);
   //Attach each instance with the corresponding pin
-  for (int i; i < numberOfMotors; i++) {
-    thetaServo[i].attach(i);
+  for (int i = 0; i < numberOfMotors; i++) {
+    thetaServo[i].attach(thetaPin[i]);
+  }
+  for (int i = 0; i < numberOfMotors; i++) {
+    thetaServo[i].write(0);
   }
 }
 int Angle;
@@ -31,6 +34,16 @@ void loop() {
    if (servoNo < numberOfMotors) {
      Angle = Serial.readStringUntil(':').toInt();
      thetaServo[servoNo].write(Angle);
+     Serial.print("Theta ");
+   Serial.print(servoNo);
+   Serial.print(" Was sent an angle of ");
+   Serial.println(Angle);
+   } else {
+    Serial.println("HERE");
+    for (int i = 0; i < numberOfMotors; i++) {
+      int val = thetaServo[i].read();
+      Serial.println(val);
+    }
    }
   }
 }
